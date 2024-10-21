f0644221aab43410c00a0a548d2c8fa0a979d645
moeux <68925026+moeux@users.noreply.github.com>
2024-10-21 14:51:36 +0200

    Added handler for dynamic voice channels.

--- Rocket/Program.cs
+++ Rocket/Program.cs
@@ -14,6 +14,7 @@ internal static class Program
     private static readonly string CommandPath = EnvironmentUtils.GetVariable("ROCKET_COMMAND_PATH", "config");
     private static readonly string LogPath = EnvironmentUtils.GetVariable("ROCKET_LOG_FILE", "rocket-.log");
     private static readonly string Token = EnvironmentUtils.GetVariable("ROCKET_DISCORD_TOKEN");
+    private static readonly string ChannelName = EnvironmentUtils.GetVariable("ROCKET_DYNAMIC_CHANNEL_NAME", "Lounge");
 
     private static readonly DiscordSocketClient Client = new(new DiscordSocketConfig
     {
@@ -88,5 +89,48 @@ internal static class Program
             await Client.SetCustomStatusAsync("Ready for takeoff!");
             await Client.SetStatusAsync(UserStatus.DoNotDisturb);
         };
+        Client.UserVoiceStateUpdated += async (_, _, state) => await RestoreVoiceChannels(state);
+    }
+
+    private static async Task RestoreVoiceChannels(SocketVoiceState state)
+    {
+        if (state.VoiceChannel.Category is not SocketCategoryChannel category
+            || category.GetPermissionOverwrite(Client.CurrentUser) is not { ManageChannel: PermValue.Allow })
+            return;
+
+        var emptyChannels = category.Channels.Where(channel => channel.Users.Count == 0).ToArray();
+
+        switch (emptyChannels.Length)
+        {
+            case > 1:
+                var orderedChannels = emptyChannels.OrderByDescending(channel => channel.Position).ToArray();
+                var removeChannels = orderedChannels[1..];
+
+                await Task.WhenAll(
+                    removeChannels.Select(channel => channel.DeleteAsync(new RequestOptions
+                    {
+                        AuditLogReason = "Obsolete dynamic voice channel"
+                    }))
+                );
+
+                break;
+            case 0:
+                await category.Guild.CreateVoiceChannelAsync(
+                    ChannelName,
+                    properties =>
+                    {
+                        var position = category.Channels.MaxBy(channel => channel.Position)?.Position;
+
+                        properties.CategoryId = category.Id;
+                        properties.Position = position ?? category.Position + 1;
+                    },
+                    new RequestOptions
+                    {
+                        AuditLogReason = "Created dynamic voice channel"
+                    }
+                );
+
+                break;
+        }
     }
 }
